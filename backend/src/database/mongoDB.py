004d86c9bced2d33ca6eadfec785eae57df03c1b
bhavnapandit <pritybhavna2001@gmail.com>
2025-06-28 13:15:09 +0530

    .env setup

--- backend/src/database/mongoDB.py
+++ backend/src/database/mongoDB.py
@@ -1,14 +1,20 @@
-
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
+from dotenv import load_dotenv
 
-uri = "mongodb+srv://bhavnapandit2001:bhavna401234@questions.nixkkwe.mongodb.net/"
+load_dotenv()
+
+import os
+
+MONGO_URI = os.getenv("MONGO_URI")
+print("MONGO_URI:", MONGO_URI)
+
 
 # Create a new client and connect to the server
-client = MongoClient(uri, server_api=ServerApi('1'))
+client = MongoClient(MONGO_URI, server_api=ServerApi("1"))
 
-db=client.languages
-collection=db["lang_ques"]
+db = client.languages
+collection = db["lang_ques"]
 user_collection = db["users"]
-#collection.insert_one({"hello": "world"})
+# collection.insert_one({"hello": "world"})
 # user_collection.insert_one({"name":"bhavna"})
